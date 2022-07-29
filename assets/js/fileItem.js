90a8787a48d50a678f8c4eb0f720120b85226948
Yago Vial <yagoavila1@gmail.com>
2022-07-29 18:02:21 -0300

    refatorado

--- assets/js/fileItem.js
+++ assets/js/fileItem.js
@@ -1,18 +1,20 @@
 
 //percorra os arquivos com file.files
 //atualizar a lista de aquivos aceitos no html, usando o atributo accept. Atualmente ele está apenas como exemplo
-
+//os arquivos usados devem ser do arrayFiles, visto que, por definição, a API file do JS é readOnly. Ou seja, não é possível a exclusão. Na array é possível
 
 let file = document.querySelector('[data-file="bfile"]')
 let bodyRow = document.querySelector('[data-file="grupo"]')
 let Row = document.querySelector('[data-file="row"]')
+const arrayFiles = []
+let k = 0
 
 file.addEventListener('input', (e) => {
     for (var i = 0; i < file.files.length; ++i) {
 
 
         // if (file.files[i] == 'text/plain') {
-
+            arrayFiles.push(file.files.item(i))
             let name = file.files.item(i).name;
             console.log("here is a file name: " + name);
 
@@ -23,18 +25,20 @@ file.addEventListener('input', (e) => {
             let close = document.createElement("button");
 
             divRow.classList.add("__row")
+            divRow.setAttribute("data-control", k)
+
             spanInputg.classList.add("__span-initial5")
 
             divg.classList.add("__input-group")
 
             text.classList.add("form-control", "__input-quite5")
-            close.classList.add("btn-close", "__close-inline", "btn-sm")
-
             text.id = "file-item-descricao" + [i]
             text.value = ""
             text.setAttribute("placeholder", "descreva o documento")
 
+            close.classList.add("btn-close", "__close-inline", "btn-sm")
             close.setAttribute("ariaLabel", "close")
+            close.setAttribute("onclick", "remove(this.parentElement.parentElement)")
 
             Row.appendChild(divRow)
             divRow.appendChild(spanInputg)
@@ -43,8 +47,15 @@ file.addEventListener('input', (e) => {
             divg.appendChild(close)
 
             spanInputg.textContent = name
+            k++
         // } else {
         //     window.alert("arquivo invalido")
         // }
     }
 })
+function remove(el) {
+    let element = el;
+    let kpai = element.dataset.control
+    arrayFiles.splice(kpai, 1, "")
+    element.remove();
+  }
