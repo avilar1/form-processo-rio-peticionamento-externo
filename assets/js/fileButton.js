2795686053ebf66daf4ed307088ba93a900a88df
Yago Vial <yagoavila1@gmail.com>
2022-08-02 16:33:17 -0300

    ajustes pontuais

--- assets/js/fileButton.js
+++ assets/js/fileButton.js
@@ -1,8 +1,25 @@
-$(function() {
-    $('#spanfile').hover(function() {
+  $(function() {
+    $('#labelbotom').hover(function() {
+
       $('#pathfile').css('fill', '#f5f5f5');
+      
     }, function() {
+
       // on mouseout, reset the background colour
       $('#pathfile').css('fill', '');
+
+    });
+  });
+
+  $(function() {
+    $('#labelbotom').hover(function() {
+
+      $('#spanfile').css('color', '#f5f5f5');
+      
+    }, function() {
+
+      // on mouseout, reset the background colour
+      $('#spanfile').css('color', '');
+
     });
   });
